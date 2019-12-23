c24858e509e6a4ea63622da481ad1d99e33b8c91
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-12-23 22:46:55 +0200

    Created base abstract Element class
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ CourseProject/Classes/Element.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    public struct Input
+    {
+        public Element Source { get; set; }
+
+        public int OutputIndex { get; set; }
+
+        public bool IsSet
+        {
+            get { return Source != null; }
+        }
+
+        public bool Value
+        {
+            get { return Source.Outputs[OutputIndex]; }
+        }
+
+        public Input(Element source, int outputIndex)
+        {
+            Source = source;
+            OutputIndex = outputIndex;
+        }
+    }
+
+    abstract public class Element
+    {
+        abstract public Input[] Inputs { get; }
+
+        abstract public bool[] Outputs { get; }
+    }
+}
