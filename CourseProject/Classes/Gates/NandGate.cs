d569f5e5f4cbba762043b7ce2f4d283224367651
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-12-24 01:03:12 +0200

    Add NAND gate class
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ CourseProject/Classes/Gates/NandGate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    class NandGate : Element
+    {
+        public override Input[] Inputs { get; }
+
+        public override bool[] Outputs
+        {
+            get
+            {
+                bool res = !Inputs.All(x => x.Value);
+
+                return new bool[] { res };
+            }
+        }
+
+        public NandGate(int numberOfInputs = 2)
+        {
+            Inputs = new Input[numberOfInputs];
+        }
+
+        public void SetInput(int inputIndex, Input input)
+        {
+            Inputs[inputIndex] = input;
+        }
+    }
+}
--- CourseProject/Program.cs
+++ CourseProject/Program.cs
@@ -17,6 +17,7 @@ namespace CourseProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CircuitDesignerForm());
+
         }
     }
 }
