e7787b83dbc6b5de42ac5e962d0b7644edb15c8b
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-12-24 00:43:20 +0200

    Add XOR gate class
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ CourseProject/Classes/Gates/XorGate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    class XorGate : Element
+    {
+        public override Input[] Inputs { get; }
+
+        public override bool[] Outputs
+        {
+            get
+            {
+                bool res = Inputs.Where(x => x.Value).Count() % 2 == 1;
+
+                return new bool[] { res };
+            }
+        }
+
+        public XorGate(int numberOfInputs = 2)
+        {
+            Inputs = new Input[numberOfInputs];
+        }
+
+        public void SetInput(int inputIndex, Input input)
+        {
+            Inputs[inputIndex] = input;
+        }
+    }
+}
