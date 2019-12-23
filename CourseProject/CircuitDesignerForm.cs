d5f062990ce4985c0a68877bca94e39537ac0b79
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-12-23 21:16:27 +0200

    Change main form name
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

--- CourseProject/CircuitDesignerForm.cs
+++ CourseProject/CircuitDesignerForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CourseProject
 {
-    public partial class Form1 : Form
+    public partial class CircuitDesignerForm : Form
     {
-        public Form1()
+        public CircuitDesignerForm()
         {
             InitializeComponent();
         }
