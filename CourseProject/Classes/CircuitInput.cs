6645c36fad97ce043133c1d7082f2c6179fe058d
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-12-23 23:21:11 +0200

    Added CircuitInput class
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ CourseProject/Classes/CircuitInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    class CircuitInput : Element
+    {
+        public bool Value { get; set; }
+
+        private Input[] inputs = new Input[0];
+        public override Input[] Inputs
+        {
+            get { return inputs; }
+        }
+
+        public override bool[] Outputs
+        {
+            get { return new bool[] { Value }; }
+        }
+
+        public CircuitInput(bool value = false)
+        {
+            Value = value;
+        }
+    }
+}
