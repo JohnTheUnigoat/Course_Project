b384a18796fc1134923fc892ad5d8d7332b99ed1
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-12-24 14:31:19 +0200

    Add XNOR gate class
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ CourseProject/Classes/Gates/XnorGate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    class XnorGate : Element
+    {
+        public override Input[] Inputs { get; }
+
+        public override bool[] Outputs
+        {
+            get
+            {
+                bool res = Inputs.Where(x => x.Value).Count() % 2 == 0;
+
+                return new bool[] { res };
+            }
+        }
+
+        public XnorGate(int numberOfInputs = 2)
+        {
+            Inputs = new Input[numberOfInputs];
+        }
+
+        public void SetInput(int inputIndex, Input input)
+        {
+            Inputs[inputIndex] = input;
+        }
+    }
+}
