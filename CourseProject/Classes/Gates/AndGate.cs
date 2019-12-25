191bde998aa64ab8202ffe97597a4e3f671963c2
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-12-25 22:09:48 +0200

    Implement Draw for every gate
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

--- CourseProject/Classes/Gates/AndGate.cs
+++ CourseProject/Classes/Gates/AndGate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,23 @@ namespace CourseProject
         }
 
         public AndGate(int numberOfInputs = 2) : base(numberOfInputs) { }
+
+        public override void Draw(Graphics gfx, Pen pen, Pen activePen, int gridSize)
+        {
+            base.Draw(gfx, pen, activePen, gridSize);
+
+            Font font = new Font("Arial", (float)(gridSize * 0.6));
+            var brush = new SolidBrush(pen.Color);
+
+            var format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+
+            Point position = new Point(Position.X * gridSize, Position.Y * gridSize + gridSize / 3);
+            Size size = new Size(gridSize * 2, gridSize * 2);
+
+            Rectangle rect = new Rectangle(position, size);
+
+            gfx.DrawString("&", font, Brushes.Wheat, rect, format);
+        }
     }
 }
--- CourseProject/Classes/Gates/NandGate.cs
+++ CourseProject/Classes/Gates/NandGate.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace CourseProject
 {
@@ -14,5 +15,31 @@ namespace CourseProject
         }
 
         public NandGate(int numberOfInputs = 2) : base(numberOfInputs) { }
+
+        public override void Draw(Graphics gfx, Pen pen, Pen activePen, int gridSize)
+        {
+            Point from = new Point(OutputPositions[0].X * gridSize - gridSize / 2, OutputPositions[0].Y * gridSize);
+            Point to = new Point(from.X - gridSize / 2, from.Y - gridSize / 3);
+
+            if (Output == true)
+                gfx.DrawLine(activePen, from, to);
+            else
+                gfx.DrawLine(pen, from, to);
+
+            base.Draw(gfx, pen, activePen, gridSize);
+
+            Font font = new Font("Arial", (float)(gridSize * 0.6));
+            var brush = new SolidBrush(pen.Color);
+
+            var format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+
+            Point position = new Point(Position.X * gridSize, Position.Y * gridSize + gridSize / 3);
+            Size size = new Size(gridSize * 2, gridSize * 2);
+
+            Rectangle rect = new Rectangle(position, size);
+
+            gfx.DrawString("&", font, Brushes.Wheat, rect, format);
+        }
     }
 }
